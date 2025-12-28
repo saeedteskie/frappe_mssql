989281ed29d03cdcf2de7fa746c39f158e8ce958
saeedteskie <38921870+saeedteskie@users.noreply.github.com>
2025-12-28 13:11:02 -0500

    Create hooks.py

---
+++ frappe_mssql/hooks.py
@@ -0,0 +1,7 @@
+app_name = "frapp_mssql"
+app_title = "frapp_mssql"
+app_publisher = "Saeed"
+app_description = "Saeed"
+app_email = "saeedt@accountedpro.com"
+app_license = "mit"
+asset_version = "1.2.37";
