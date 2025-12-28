79d16c6adc6dbf8c7036cba09f15eb5396842a7f
saeedteskie <38921870+saeedteskie@users.noreply.github.com>
2025-12-28 13:11:16 -0500

    Delete saeed_customizations directory

--- saeed_customizations/desk/doctype/event/event_calendar.js
+++
@@ -1,8 +0,0 @@
-frappe.views.calendar["Event"] = {
-    onload: function(calendar_view) {
-        console.log("Calendar View Loaded");
-    },
-    refresh: function(calendar_view) {
-        console.log("Calendar View Refreshed");
-    }
-};
